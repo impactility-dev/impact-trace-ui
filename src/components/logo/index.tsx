164dc0fa9afeaa581a5d12c63aec0bc1b1a169ee
Sachin Mesare <sachin.mesare@impactility.com>
2023-10-20 14:53:27 +0530

    - Added Shell Logo

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -21,7 +21,7 @@ interface Props {
 
 const LogoSection = ({ reverse, isIcon, sx, to }: Props) => (
   <ButtonBase disableRipple component={Link} to={!to ? APP_DEFAULT_PATH : to} sx={sx}>
-    {isIcon ? <LogoIcon /> : <Logo reverse={reverse} />}
+    {isIcon ? <Logo /> : <Logo reverse={reverse}/>}
   </ButtonBase>
 );
 
